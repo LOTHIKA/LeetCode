00b5da99c2fd74dddb7b2eab57099423723d6ff8
LOTHIKA R G <167205090+LOTHIKA@users.noreply.github.com>
2024-08-17 09:56:17 +0530

    Time: 6 ms (60.05%) | Memory: 43.1 MB (63.24%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -5,20 +5,13 @@ class Solution {
        for(int i=0;i<n;i++){
            str[i] = String.valueOf(nums[i]);
        }
-       Arrays.sort(str, new Comparator<String>()
-       {
-            public int compare(String s1, String s2){
-                String left = s1+s2;
-                String right = s2+s1;
-                return right.compareTo(left);
-            }
-       });
+       Arrays.sort(str, (a,b)->(b+a).compareTo(a+b));
        StringBuilder sb = new StringBuilder();
        for(String num : str){
            sb.append(num);
        }
-       while(sb.charAt(0)=='0' && sb.length()>1 ){
-        sb.deleteCharAt(0);
+       if(sb.charAt(0)=='0'){
+        return "0";
        }
        return sb.toString();
     }

eeaef2c2f9867c8fc037670639790d8424b78020
LOTHIKA R G <167205090+LOTHIKA@users.noreply.github.com>
2024-08-17 14:49:43 +0530

    Time: 609 ms (13.50%) | Memory: 45.4 MB (11.25%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,11 +1,37 @@
 class Solution {
+
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
     public String shortestPalindrome(String s) {
-        StringBuilder sb = new StringBuilder(s).reverse();
-        for(int i=0;i<=s.length();i++){
-            if(s.startsWith(sb.substring(i))){
-                return sb.substring(0,i) + s;
+
+        if (s.length() <= 1) {
+            return s;
+        }
+
+    
+        int end = 0;
+        for (int i = s.length(); i >= 0; i--) {
+            if (isPalindrome(s.substring(0, i))) {
+                end = i;
+                break;
             }
         }
-        return "";
+
+        
+        String suf = s.substring(end);
+        String rev = new StringBuilder(suf).reverse().toString();
+
+        
+        return rev + s;
     }
 }

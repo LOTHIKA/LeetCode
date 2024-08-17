f61d8d0a681cf34bd83bf308c06b4517c5997270
LOTHIKA R G <167205090+LOTHIKA@users.noreply.github.com>
2024-08-17 10:37:04 +0530

    Time: 36 ms (37.19%) | Memory: 45.4 MB (7.79%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String shortestPalindrome(String s) {
+        StringBuilder sb = new StringBuilder(s).reverse();
+        for(int i=0;i<=s.length();i++){
+            if(s.startsWith(sb.substring(i))){
+                return sb.substring(0,i) + s;
+            }
+        }
+        return "";
+    }
+}
